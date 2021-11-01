142a299964dcfe51000a5e40e3029adca7e1d687
zsw <zsw0407@outlook.com>
2021-11-01 16:48:20 +0800

    update blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   dest: 'blog',
   theme: 'reco',
-  title: "latte and cat",
+  title: 'latte and cat',
   description: 'study programs and record life',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
@@ -22,8 +22,8 @@ module.exports = {
       { text: 'Home', link: '/', icon: 'reco-home' },
       { text: 'Notes', link: '/note/', icon: 'reco-document' },
       { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-      { text: 'PhotoAlbum', link: 'https://photo.smallsunnyfox.com/', icon: 'reco-blog'},
-      { text: 'About', link: '/about/', icon: 'reco-account'},
+      { text: 'PhotoAlbum', link: 'https://photo.smallsunnyfox.com/', icon: 'reco-blog' },
+      { text: 'About', link: '/about/', icon: 'reco-account' },
       { text: 'GitHub', link: 'https://github.com/smallsunnyfox', icon: 'reco-github' }
     ],
     friendLink: [
@@ -36,13 +36,13 @@ module.exports = {
       {
         title: 'vuepress-theme-reco',
         desc: 'A simple and beautiful vuepress Blog & Doc theme.',
-        logo: "https://photo.smallsunnyfox.com/images/blog/friendlink/theme_reco.png",
+        logo: 'https://photo.smallsunnyfox.com/images/blog/friendlink/theme_reco.png',
         link: 'https://vuepress-theme-reco.recoluan.com'
       },
       {
         title: 'L1nSn0w’s Blog',
         desc: '一名热爱计算机与iOS的高中生',
-        logo: "https://photo.smallsunnyfox.com/images/blog/friendlink/lin_snow.png",
+        logo: 'https://photo.smallsunnyfox.com/images/blog/friendlink/lin_snow.png',
         link: 'https://l1nsn0w.gitee.io'
       }
     ],
@@ -70,56 +70,32 @@ module.exports = {
         {
           title: 'HTML5',
           collapsable: true,
-          children: [
-            'html5/HTML5的语义元素',
-            'html5/HTML5多媒体标签',
-            'html5/HTML5表单元素',
-            'html5/HTML5中的API',
-            'html5/Canvas'
-          ]
+          children: ['html5/HTML5的语义元素', 'html5/HTML5多媒体标签', 'html5/HTML5表单元素', 'html5/HTML5中的API', 'html5/Canvas']
         },
         {
           title: 'CSS',
           collapsable: true,
-          children: [
-            'css/css3新特性',
-            'css/css3Flip'
-          ]
+          children: ['css/css3新特性', 'css/css3Flip']
         },
         {
           title: 'JS',
           collapsable: true,
-          children: [
-            'js/js数据类型',
-            'js/js对象',
-            'js/js继承',
-            'js/js原型链、闭包',
-            'js/js函数的四种调用方式'
-          ]
+          children: ['js/js数据类型', 'js/js对象', 'js/js继承', 'js/js原型链、闭包', 'js/js函数的四种调用方式']
         },
         {
           title: 'Vue.js',
           collapsable: true,
-          children: [
-            'Vue/Vue基础笔记',
-            'Vue/Vue组件'
-          ]
+          children: ['Vue/Vue基础笔记', 'Vue/Vue组件']
         },
         {
           title: '前端单元测试',
           collapsable: true,
-          children: [
-            'fe-unit-test/chai',
-            'fe-unit-test/mocha',
-            'fe-unit-test/vueTestUtils'
-          ]
+          children: ['fe-unit-test/chai', 'fe-unit-test/mocha', 'fe-unit-test/vueTestUtils']
         },
         {
           title: '微信小程序',
           collapsable: true,
-          children: [
-            'wechat-mini-program/初识微信小程序',
-          ]
+          children: ['wechat-mini-program/初识微信小程序']
         }
       ]
     },
@@ -139,7 +115,7 @@ module.exports = {
      * valine 设置 (if you need valine comment )
      */
     valineConfig: {
-      appId: 'XDwlSXS2pD137bPrPpwQaqqD-gzGzoHsz',// your appId
+      appId: 'XDwlSXS2pD137bPrPpwQaqqD-gzGzoHsz', // your appId
       appKey: 'CQ8FKrMUP76LwycPcKlDoRqV', // your appKey
       placeholder: '是时候展现真正的技术了',
       avatar: 'wavatar',
@@ -180,26 +156,25 @@ module.exports = {
           {
             name: '我再没见过 像你一般的星空',
             artist: 'Seto',
-            url: 'https://assets.smallsunnyfox.com/music/Seto我再没见过像你一般的星空.mp3',
-            cover: 'https://assets.smallsunnyfox.com/music/1.png'
+            url: 'https://assets.smallsunnyfox.com/music/2.mp3',
+            cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
           },
           {
             name: '萤火之森',
             artist: 'CMJ',
-            url: 'https://assets.smallsunnyfox.com/music/CMJ萤火之森.mp3',
-            cover: 'https://assets.smallsunnyfox.com/music/2.png'
+            url: 'https://assets.smallsunnyfox.com/music/3.mp3',
+            cover: 'https://assets.smallsunnyfox.com/music/3.jpg'
           }
         ]
       }
-    ]
-    ,
+    ],
     [
-      '@vuepress/pwa', 
+      '@vuepress/pwa',
       {
         serviceWorker: true,
         updatePopup: {
-          message: "发现新内容可用",
-          buttonText: "刷新"
+          message: '发现新内容可用',
+          buttonText: '刷新'
         }
       }
     ],
@@ -211,4 +186,4 @@ module.exports = {
       }
     ]
   ]
-}  
+}
